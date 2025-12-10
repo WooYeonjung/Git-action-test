913ce0d3775b57165341d0854502091b7f897f62
Ellie <161689001+WooYeonjung@users.noreply.github.com>
2025-12-10 15:56:49 +0900

    Update ModifyPage.js
    ModifyPage 의 useCallback 추가 및 수정

--- src/pages/todo/ModifyPage.js
+++ src/pages/todo/ModifyPage.js
@@ -2,17 +2,22 @@ import { useNavigate } from "react-router-dom";
 
 const ModifyPage = ({ tno }) => {
     const navigate = useNavigate();
-    const moveToRead = () => {
+    const moveToRead = useCallback(() => {
         navigate({ pathname: `/todo/read/${tno}` })
-    }
-    const moveToList = () => {
+    },[navigate])
+    const moveToList =  useCallback(() => {
         navigate({ pathname: `/todo/list` })
-    }
+    },[navigate])
 
 
     return (
         <div className="text-3xl font-extrabold">
             Todo Modify Page
+        <div onClick={moveToRead}>
+          moveToRead
+        </div>
+         <div onClick={moveToList}>
+        </div>
         </div>
     );
 }
